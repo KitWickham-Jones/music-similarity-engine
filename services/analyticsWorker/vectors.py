9469ddfdcfe6ff70a0b8ae535c57b00bfb41f2d2
Kit Wickham-Jones <kitwj12@gmail.com>
2026-06-06 16:27:59 +0300

    service worker class

--- services/analyticsWorker/vectors.py
+++ services/analyticsWorker/vectors.py
@@ -1,52 +1,53 @@
 from transformers import ClapModel, ClapProcessor
 import torchaudio
 import torch
+from torch import Tensor
 import time
+import logging
+import contextlib
+import os
 
-print("Loading CLAP model........\n")
-start = time.time()
-model = ClapModel.from_pretrained("laion/clap-htsat-unfused")
-processor = ClapProcessor.from_pretrained("laion/clap-htsat-unfused")
-print(f"Loaded CLAP model in {time.time() - start:.2f}s\n")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
-print("Loading song........\n")
-start = time.time()
-
-waveform, sr = torchaudio.load("/Users/kitwj/Documents/Personal/Projects/QueuedMusicAnalysis/jungle.mp3")
-print(f"Loaded audio into memory in {time.time() - start:.2f}s\n")
-
-#CLAP is trained at 48000hz so rescaling songs if needed
-if sr != 48000:
-	print("Rescaling audio file to 48000h........\n")
+@contextlib.contextmanager
+def log_time(label):
 	start = time.time()
-	resampler = torchaudio.transforms.Resample(sr, 48000)
-	waveform = resampler(waveform)
-	print(f"Rescaled audio in {time.time() - start:.2f}s")
+	yield
+	logger.info(f"{label} in {time.time() - start:.2f}s")
 
-start = time.time()
-print(f"Converting waveform to numpy........\n")
+class VectorGenerator:
+	def __init__(self):
+		with log_time("Loaded CLAP Model"):
+			self.model = self.model = ClapModel.from_pretrained("laion/clap-htsat-unfused")
+		with log_time("Loading CLAP Processor"):
+			self.processor = ClapProcessor.from_pretrained("laion/clap-htsat-unfused")
+		self.sr = None
+		self.waveform = None
 
-y = waveform.mean(dim=0).numpy()
+	def load_song(self, songPath: str, jobID: str):
+		try:
+			if not os.path.exists(songPath):
+				raise FileNotFoundError(f"Audio file not found at: {songPath}")
+			with log_time("Loaded song"):
+				self.waveform, self.sr = torchaudio.load(songPath)
+			if self.sr != 48000:
+				self.rescale_song(self.waveform, self.sr)
+		except Exception as e:
+			logger.error(f"Job {jobID} failed: {e}")
 
-print(f"Converted in {time.time() - start:.2f}s")
-
-
-start = time.time()
-print(f"Applying CLAP processor........\n")
-
-inputs = processor(audio=y, return_tensors="pt", sampling_rate=48000)
-
-print(f"Applied processor in {time.time() - start:.2f}s\n")
-
-start = time.time()
-print(f"Extracting audio features........\n")
-
-with torch.no_grad():
-	embedding = model.get_audio_features(**inputs)
-	print(f"Embedded shape of tensor: {embedding.pooler_output.shape}")
-
-vector = embedding[0].numpy().tolist()
-
-print(f"Extracted features in {time.time() - start:.2f}s\n")
-print(f"Embedded vector dimensions: {len(vector[0])}\n")
+	def rescale_song(self, waveform: Tensor, sr: int):
+		resampler = torchaudio.transforms.Resample(sr, 48000)
+		with log_time("Rescaled waveform"):
+			self.waveform = resampler(waveform)
+	
+	def extract_embedding(self):
+		y = self.waveform.mean(dim=0).numpy()
+		with log_time("Processor"):
+			inputs = self.processor(audio=y, return_tensors="pt", sampling_rate=48000)
+		with log_time("CLAP inferences"), torch.no_grad():
+			embedding = self.model.get_audio_features(**inputs)
+		return embedding.pooler_output[0].numpy().tolist()	
 
